9edb056cd1fe29192f04b4730f2bd00484a8f269
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-09-09 03:40:01 -0700

    Add voice chat command declarations

---
+++ client/command/voice.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "command.h"
+
+ChimeraCommandError voice_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_all_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_team_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_host_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_ptt_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_speakers_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_volume_command(size_t argc, const char **argv) noexcept;
