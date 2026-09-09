1c211ea73c6c2e4fd7ec344849489ae206125259
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-09-09 03:40:41 -0700

    Complete voice command declarations

--- client/command/voice.h
+++ client/command/voice.h
@@ -8,3 +8,5 @@ ChimeraCommandError voice_host_command(size_t argc, const char **argv) noexcept;
 ChimeraCommandError voice_ptt_command(size_t argc, const char **argv) noexcept;
 ChimeraCommandError voice_speakers_command(size_t argc, const char **argv) noexcept;
 ChimeraCommandError voice_volume_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError voice_status_command(size_t argc, const char **argv) noexcept;
+void set_up_voice_connection_watcher() noexcept;
