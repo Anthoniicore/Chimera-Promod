6738ea645507a42b3eb5abac3fce375765852eb2
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-09-09 03:39:54 -0700

    Expose team game state for voice chat

--- client/halo_data/server.h
+++ client/halo_data/server.h
@@ -2,18 +2,9 @@
 
 #include <stdint.h>
 
-typedef enum ServerType : uint16_t {
-    SERVER_NONE = 0,
-    SERVER_DEDICATED = 1,
-    SERVER_LOCAL = 2
-} ServerType;
+typedef enum ServerType : uint16_t { SERVER_NONE = 0, SERVER_DEDICATED = 1, SERVER_LOCAL = 2 } ServerType;
 ServerType server_type();
 
-typedef enum Gametype : uint16_t  {
-    GAMETYPE_CTF = 1,
-    GAMETYPE_SLAYER = 2,
-    GAMETYPE_ODDBALL = 3,
-    GAMETYPE_KING = 4,
-    GAMETYPE_RACE = 5
-} Gametype;
+typedef enum Gametype : uint16_t { GAMETYPE_CTF = 1, GAMETYPE_SLAYER = 2, GAMETYPE_ODDBALL = 3, GAMETYPE_KING = 4, GAMETYPE_RACE = 5 } Gametype;
 Gametype gametype();
+bool is_team();
